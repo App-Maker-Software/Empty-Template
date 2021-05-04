1649d6f9f83b6fdbd817dca8173e803cfefbe0cb
Joe Hinkle <joehinkle11@gmail.com>
2021-05-03 20:45:58 -0600

    first commit

---
+++ Empty Template/Empty_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empty_TemplateApp.swift
+//  Empty Template
+//
+//  Created by Joseph Hinkle on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Empty_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
